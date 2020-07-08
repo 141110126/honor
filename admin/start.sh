daa62afd13413469efbd65746f914dc9e76b1b59
jiwanying <1564674551@qq.com>
2020-07-08 19:15:41 +0800

    修改linux脚本文件

--- admin/start.sh
+++ admin/start.sh
@@ -6,6 +6,7 @@ npm run build
 # 删除容器
 docker rm -f honor-admin
 # 启动容器
-docker run -d -p 8080:80 -v $PWD/dist:/usr/share/nginx/html nginx
+# docker run -d -p 8080:80 -v $PWD/dist:/usr/share/nginx/html nginx
+docker run -d -p 8080:80 -v $(cd $(dirname $0); pwd)/dist:/usr/share/nginx/html nginx
 
 # docker run -d --restart=on-failure:5 -p 8080:80 -v $PWD/dist:usr/share/nginx/html --name honor-admin nginx
