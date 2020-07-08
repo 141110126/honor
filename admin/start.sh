602ca5d0f26ccbc0893e073c10f93a07549554d1
jiwanying <1564674551@qq.com>
2020-07-08 19:23:29 +0800

    修改linux脚本文件

--- admin/start.sh
+++ admin/start.sh
@@ -1,10 +1,15 @@
 # linux脚本文件
 # 拉取代码
 git pull
+
 # 打包
 npm run build
+
 # 删除容器
 docker rm -f honor-admin
+
+echo $(cd $(dirname $0); pwd)
+
 # 启动容器
 # docker run -d -p 8080:80 -v $PWD/dist:/usr/share/nginx/html nginx
 docker run -d -p 8080:80 -v $(cd $(dirname $0); pwd)/dist:/usr/share/nginx/html nginx
