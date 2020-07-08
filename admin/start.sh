287117d2ebbb0b7b2c0b754c16448ad631ad379f
jiwanying <1564674551@qq.com>
2020-07-08 18:29:35 +0800

    添加linux脚本文件

---
+++ admin/start.sh
@@ -0,0 +1,9 @@
+# linux脚本文件
+# 拉取代码
+git pull
+# 打包
+npm run build
+# 删除容器
+docker rm -f honor-admin
+# 启动容器
+docker run -d --restart=on-failure:5 -p 8080:80 -v $PWD/dist:usr/share/nginx/html --name honor-admin nginx
